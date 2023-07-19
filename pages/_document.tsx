5721880c4dea47d0bc8d97d669da1754a4282422
alexjsanches <alexandrej.sanches@gmail.com>
2023-07-19 17:23:04 -0300

    tag facebook

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <Html>
         <Head>
+        <meta property="og:title" content="B Mais Saúde" />
+          <meta property="og:description" content="Planos de saúde e odontológicos" />
           <link rel="icon" href="favicon/favicon.ico" />
+          <meta name="facebook-domain-verification" content="jf5fsutqw4u76q01h4q9osrk9v1hzc" />
         </Head>
         <body>
           <Main />
